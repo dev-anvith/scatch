1ccf3f2327d098459fc6b982043585d879a72706
dev-anvith <anvithrh@gmail.com>
2025-05-04 13:55:38 +0530

    Changed MongoDB connection config

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
-const config = require('config')
-const dbgr = require('debug')("development:mongoose");
-
+require("dotenv").config();
+// Ensuring the environment variable exists
+if (!process.env.MONGODB_URI) {
+    console.error("MONGODB_URI is not defined in your environment.");
+    process.exit(1); // Exit if the connection URI is not found
+}
 
 mongoose
-.connect(`${config.get("MONGODB_URI")}/scatch`)
-.then(function(){
-    console.log("Connected");
-    
-})
-.catch(function(err){
-    console.log(err);
-    
-    
-});
+    .connect(`${process.env.MONGODB_URI}/scatch`, { useNewUrlParser: true, useUnifiedTopology: true }) // Added options for compatibility
+    .then(() => {
+        console.log("Connected to MongoDB successfully!");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // Exit process if connection fails
+    });
 
 module.exports = mongoose.connection;
